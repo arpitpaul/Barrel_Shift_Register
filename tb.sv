07539b2ff1a1d7716b598b60739c838521f3e425
Arpit Paul <111978808+arpitpaul@users.noreply.github.com>
2024-05-03 18:46:12 +0530

    Update tb.sv

--- tb.sv
+++ tb.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Er. Arpit Paul
 // 
 // Create Date: 03.05.2024 17:52:11
 // Design Name: 
